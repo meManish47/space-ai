645e30ccf487a7ae3e7e0d7e6a140d2958889c95
Manish Kumar <kmanish57610@gmail.com>
2026-02-06 22:19:53 +0530

    Updated few changes for smooth deployment

--- src/services/gqlclient.ts
+++ src/services/gqlclient.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
 const endpoint =
-  process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ??
+  process.env.NEXT_PUBLIC_HOST_NAME ??
   "http://localhost:3000/api/graphql";
 
 export const gqlClient = new GraphQLClient(endpoint);
